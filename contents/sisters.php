c48ed1d5303e0de6be650c402c7793b16de44e26
Mostafa Sabiloo <mostafasabiloo@gmail.com>
2020-08-02 13:35:49 +0430

    Remove inactive lugs
    Remove: Yazd lug, Tabriz lug, Isfahan lug, Zanjan lug and Guilan lug url

--- contents/sisters.php
+++ contents/sisters.php
@@ -1,19 +1,3 @@
-<a href="http://www.isfahanlug.org/doku.php">
-	<img src="images/lugs/isflug.jpg" class="photo" />
-</a>
-
-<a href="http://guilug.org/">
-	<img src="images/lugs/guilug.png" class="photo" />
-</a>
-
-<a href="http://tabrizlug.com/">
-	<img src="images/lugs/tabriz.png" class="photo" />
-</a>
-
-<a href="http://yazdlug.net/forum/">
-	<img src="images/lugs/yazdlug.jpg" class="photo" alt="گروه کاربران گنو/لینوکس یزد" />
-</a>
-
 <a href="http://www.mashhadlug.org/">
 	<img src="images/lugs/mashlug.jpg" class="photo" />
 </a>
@@ -24,10 +8,6 @@
 
 <a href="http://karajlug.org/">
 	<img src="images/lugs/karajlug.png" class="photo" />
-</a>
-
-<a href="http://www.zanjanlug.org/">
-	<img src="images/lugs/zanjanlug.png" class="photo" />
 </a>
 
 <br />
